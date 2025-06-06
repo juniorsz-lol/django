f999317a4e84801b4bee88cba1204662fe41ba17
juniorsz-lol <jose.suntura@uab.edu.bo>
2025-06-05 23:50:03 -0400

    django

--- my_first_project/my_first_project/urls.py
+++ my_first_project/my_first_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import myView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('car-list/', myView )
 ]
